dcb7a5408912a6c21dc2e5491720f150f2038109
cbrett@me.com <User@DESKTOP-0LKT4QP>
2026-07-07 15:20:37 +0100

    fix: correct test structure by closing navigate to Dyson home page test

--- tests/first.test.spec.ts
+++ tests/first.test.spec.ts
@@ -9,6 +9,7 @@ test('navigate to Dyson home page', async ({ page }) => {
   await page.getByRole('tab', { name: 'Manufacturers' }).click();
   await page.getByRole('link', { name: 'Dyson Dyson Technology for' }).click();
   await expect(page).toHaveURL('https://source.thenbs.com/en/gb/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
+});
 
 test('check H1 heading', async ({ page }) => {
   await page.goto('https://source.thenbs.com/en/gb');
